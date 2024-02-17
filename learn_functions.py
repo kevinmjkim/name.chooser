e9905efad3d80e0dcd650d9869510bbf66b4b018
kevinmjkim <132722045+kevinmjkim@users.noreply.github.com>
2024-02-16 19:39:49 -0600

    Add files via upload

---
+++ learn_functions.py
@@ -0,0 +1,38 @@
+import time
+def my_math2(a,b):
+    return 2*a*a - 5*b
+
+print(my_math2(7987980099079797, 100928))
+
+def kim(x,y,z):
+    return (4*x**2+7**y)/(3*z**x+2*x)
+print(kim(0,0,0))
+
+def pig_latin(word):
+   new_word = word[1:]
+   new_word = new_word+ word[0]
+   new_word = new_word + "ay"
+   return new_word
+print(pig_latin("\t😀😃😄😁😆😅😂🤣🥲🥹😊😇🙂🙃😉😌😍🥰😘😗😙😚😋😛😝😜🤪🤨🧐🤓😎🥸🤩🥳😏😒😞😔😟😕🙁😣😖😫😩🥺😢😭😮💨😤😠😡🤬🤯😳🥵🥶😱😨😰😥😓🫣🤗🫡🤔🫢🤭🤫🤥😶😶🌫️😐😑😬🫨🫠🙄😯😦😧😮😲🥱😴🤤😪😵😵💫🫥🤐🥴🤢🤮🤧😷🤒🤕🤑🤠😈👿👹👺🤡💩👻💀👽👾🤖🎃😺😸😹😻😼😽🙀😿😾"))
+
+def sun(a,b, c):
+    return ("Am I truly"+ a+ "happy or not. "+ "If not" + b+ ". TELL ME WHY:" + a + c)
+print(sun(" sad ", " get a life ", ", need life"))
+
+def blooper(a,b):
+    return("Part 1 : What does" + a + " + " + b + "You get " + a + b + "I 54don't 67890know 76543256789how 0876543212I 3456789got 76543this 21 number345678")
+print(blooper("*******","****"))
+
+def Dooper(a,b):
+    return("Part 2:What does" + a + " + " + b + "You get " + a + b + "I 54don't 678asdf sf sf (is this possible)sf sdf ssadf df a90know 7654325dfdfd6789how 0efefeere876543212I 3456789got 76543this 21 number345678 a")
+print(blooper("😮💨😤😠😡🤬🤯😳🥵🥶😱😨😰😥😓","🤣🥲🥹😊😇🙂🙃😉😌😍"))
+
+def nonpossiblypotic(a,b):
+    return 2**8765432 - a - b
+
+print(my_math2(7987980099079797, 100928))
+
+def clown_apocolyse(a,b):
+    return "🤡 " + a + " + " + b + "  # of clowns"
+    
+print(zombie_apocolyse("99999999999999999999999", "987654323456789876543234567897654321234567898765432123456789"))
